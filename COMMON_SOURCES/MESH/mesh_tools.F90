e7fb3626380890f3cd24f2aec80d6a864a0f17f0
mcreff <melvin.creff@gmail.com>
2025-04-28 11:28:16 -0500

    test

--- COMMON_SOURCES/MESH/mesh_tools.F90
+++ COMMON_SOURCES/MESH/mesh_tools.F90
@@ -92,6 +92,7 @@ CONTAINS
       mesh2%isolated_jjs = mesh1%isolated_jjs
       ALLOCATE(mesh2%isolated_interfaces(SIZE(mesh1%isolated_interfaces, 1), SIZE(mesh1%isolated_interfaces, 2)))
       mesh2%isolated_interfaces = mesh1%isolated_interfaces
+      write(*,*) '?'
    END SUBROUTINE copy_mesh
 
    SUBROUTINE free_mesh(mesh)
