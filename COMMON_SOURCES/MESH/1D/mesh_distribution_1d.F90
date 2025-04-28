858f83879d04f4ba579732a3507412b43032a473
mcreff <melvin.creff@gmail.com>
2025-04-28 14:42:45 -0500

    added per to multiple prod mesh distr

--- COMMON_SOURCES/MESH/1D/mesh_distribution_1d.F90
+++ COMMON_SOURCES/MESH/1D/mesh_distribution_1d.F90
@@ -133,9 +133,6 @@ CONTAINS
       IF (rank == 1) THEN
          mesh_loc%i_d = mesh_glob%i_d(me_start:me_end)
          mesh_loc%jj = mesh_glob%jj(:, me_start:me_end) - np_start + 1
-         IF (per_bool) THEN
-            mesh_loc%jj = mesh_loc%jj - 1
-         END IF
          mesh_loc%rr(:, 1:mesh_loc%dom_np) = mesh_glob%rr(:, np_start:np_end)
 
          IF (per_bool) THEN
@@ -157,7 +154,7 @@ CONTAINS
          IF (per_bool) THEN
             mesh_loc%i_d(1:mesh_loc%me - 1) = mesh_glob%i_d(me_start:me_end - 1)
             mesh_loc%i_d(mesh_loc%me) = mesh_glob%i_d(1)
-            mesh_loc%jj(:, 1:mesh_loc%dom_np - 1) = mesh_glob%jj(:, me_start:me_end - 1) - np_start
+            mesh_loc%jj(:, 1:mesh_loc%dom_np - 1) = mesh_glob%jj(:, me_start:me_end - 1) - np_start + 1
             mesh_loc%jj(2, mesh_loc%dom_np) = mesh_glob%np
             mesh_loc%rr(:, 1:mesh_loc%dom_np - 1) = mesh_glob%rr(:, np_start:np_end - 1)
             mesh_loc%rr(:, mesh_loc%dom_np) = mesh_glob%rr(:, 1)
@@ -205,9 +202,6 @@ CONTAINS
       ELSE
          mesh_loc%i_d = mesh_glob%i_d(me_start:me_end)
          mesh_loc%jj = mesh_glob%jj(:, me_start:me_end) - np_start + 1
-         IF (per_bool) THEN
-            mesh_loc%jj = mesh_loc%jj - 1
-         END IF
          mesh_loc%rr(:, 1:mesh_loc%dom_np) = mesh_glob%rr(:, np_start:np_end)
 
          mesh_loc%rr(:, mesh_loc%np) = mesh_glob%rr(:, np_start - 1)
