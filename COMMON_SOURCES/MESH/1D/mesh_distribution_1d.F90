0eceedb0a1069141978dafb5aee267c2a480d145
mcreff <melvin.creff@gmail.com>
2025-04-28 14:39:56 -0500

    added per to multiple prod mesh distr

--- COMMON_SOURCES/MESH/1D/mesh_distribution_1d.F90
+++ COMMON_SOURCES/MESH/1D/mesh_distribution_1d.F90
@@ -244,6 +244,7 @@ CONTAINS
       DO n = 1, nb_procs
          mesh_loc%discell(n + 1) = mesh_loc%discell(n) + mesh_loc%domcell(n)
       END DO
+      write(*,*) rank, mesh%jj
 
    END SUBROUTINE extract_mesh_1d
 
