13f887bd15bc30338e865a34eb845ea18b03fcfb
mcreff <melvin.creff@gmail.com>
2025-04-30 17:10:17 -0500

    added better bc

--- COMMON_SOURCES/MESH/dirichlet_data_module.F90
+++ COMMON_SOURCES/MESH/dirichlet_data_module.F90
@@ -45,7 +45,7 @@ CONTAINS
       USE input_dirichlet_data
       USE dirichlet_data_module
       IMPLICIT NONE
-      CLASSdirichlet_bc) :: this
+      CLASS(dirichlet_bc) :: this
       TYPE(mesh_type) :: mesh
       CHARACTER(100) :: name
       LOGICAL, DIMENSION(:), POINTER :: virgin
