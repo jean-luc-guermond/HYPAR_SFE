74eb23360defe59900c505865e1cf8476dd197d2
mcreff <melvin.creff@gmail.com>
2025-05-01 18:47:06 -0500

    time step

--- PROBLEM_SOURCES/EULER/euler_type_module.F90
+++ PROBLEM_SOURCES/EULER/euler_type_module.F90
@@ -18,6 +18,8 @@ MODULE euler_type_MODULE
 
    ABSTRACT INTERFACE
       SUBROUTINE function_template_impose_bc(un, euler_bc, mesh, time)
+         USE petsc
+         USE def_type_mesh
          REAL(KIND = 8), DIMENSION(:, :), INTENT(INOUT) :: un
          TYPE(mesh_type) :: mesh
          TYPE(euler_bc_type) :: euler_bc
