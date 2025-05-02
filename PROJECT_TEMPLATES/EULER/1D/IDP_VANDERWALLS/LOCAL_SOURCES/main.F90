a1a99cc532f3eb976521410804c0bb5b4d2d36f3
mcreff <melvin.creff@gmail.com>
2025-05-02 16:10:57 -0500

    added vdw

--- PROJECT_TEMPLATES/EULER/1D/IDP_VANDERWALLS/LOCAL_SOURCES/main.F90
+++ PROJECT_TEMPLATES/EULER/1D/IDP_VANDERWALLS/LOCAL_SOURCES/main.F90
@@ -15,7 +15,7 @@ PROGRAM prog
 
    CALL plot_1d(euler%mesh%rr(1, :), un(:,1), 'initrho' // trim(adjustl(car)) // '.plt')
 
-   euler%cfl = 0.5
+   euler%cfl = 0.1
 
    DO WHILE (euler%time < 0.1 )
       CALL euler%update(un)
