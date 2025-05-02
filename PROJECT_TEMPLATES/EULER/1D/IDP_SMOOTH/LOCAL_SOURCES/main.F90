bbee20443e6635f2c81ad790c28035d890d2c341
mcreff <melvin.creff@gmail.com>
2025-05-01 19:07:06 -0500

    time step

--- PROJECT_TEMPLATES/EULER/1D/IDP_SMOOTH/LOCAL_SOURCES/main.F90
+++ PROJECT_TEMPLATES/EULER/1D/IDP_SMOOTH/LOCAL_SOURCES/main.F90
@@ -1,19 +1,23 @@
 PROGRAM prog
 #include "petsc/finclude/petsc.h"
-  USE start_setup_MODULE
-  USE sub_plot
-  IMPLICIT NONE
-  REAL(KIND=8), DIMENSION(:,:), ALLOCATABLE :: un
-  CHARACTER(5) :: car
-  
-  CALL start_setup
- 
-  WRITE(car,'(I5)') rank
-  ALLOCATE(un(mesh%np, 3))
-  un(:,1) = euler%mesh%rr(1,:)**2 + 1
-  un(:,2) = SIN(euler%mesh%rr(1,:))
-  un(:,3) = euler%mesh%rr(1,:)**2 + 2
-  !inputs%time =0.d0
-  !CALL init(un,mesh%rr)
-  CALL plot_1d(euler%mesh%rr(1,:),euler%pressure(un),'p' // trim(adjustl(car)) // '.plt')
+   USE start_setup_MODULE
+   USE sub_plot
+   IMPLICIT NONE
+   REAL(KIND = 8), DIMENSION(:, :), ALLOCATABLE :: un
+   CHARACTER(5) :: car
+   INTEGER :: n
+
+   CALL start_setup
+
+   WRITE(car, '(I5)') rank
+   ALLOCATE(un(mesh%np, 3))
+   CALL init(un, 0.d0, euler%mesh%rr)
+
+   euler%dt = 0.5d0 / euler%mesh%np
+
+   DO n = 1, 5
+      CALL euler%update(un)
+   END DO
+
+   CALL plot_1d(euler%mesh%rr(1, :), euler%pressure(un), 'p' // trim(adjustl(car)) // '.plt')
 END PROGRAM prog
