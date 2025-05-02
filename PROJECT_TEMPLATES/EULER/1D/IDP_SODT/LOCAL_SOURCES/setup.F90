0beaaf51c43cf3de75cbe0339456897b4e2729ae
mcreff <melvin.creff@gmail.com>
2025-05-02 14:39:46 -0500

    added IDP_SODT

--- PROJECT_TEMPLATES/EULER/1D/IDP_SODT/LOCAL_SOURCES/setup.F90
+++ PROJECT_TEMPLATES/EULER/1D/IDP_SODT/LOCAL_SOURCES/setup.F90
@@ -4,6 +4,9 @@ MODULE setup
    PUBLIC :: sol_anal, init, rho_anal, press_anal, mt_anal, E_anal, impose_bc_euler
    PRIVATE
    REAL(KIND = 8) :: x0, x1
+   REAL(KIND = 8) :: rhol, pl, ul
+   REAL(KIND = 8) :: rhor, pr, ur
+   REAL(KIND = 8) :: long, x0
 CONTAINS
 
    SUBROUTINE impose_bc_euler(un, euler_bc, mesh, time)
@@ -28,15 +31,13 @@ CONTAINS
 
    END SUBROUTINE impose_bc_euler
 
-   SUBROUTINE init(un, rr)
+   SUBROUTINE init(un, time, rr)
       USE def_of_gamma
       USE lambda_module
       IMPLICIT NONE
       REAL(KIND = 8), DIMENSION(:, :), INTENT(IN) :: rr
       REAL(KIND = 8), DIMENSION(SIZE(rr, 2), k_dim + 2), INTENT(OUT) :: un
-      REAL(KIND = 8) :: cl, cr, rho_plus
-      REAL(KIND = 8) :: in_state(2), in_data(3), out_state(4)
-
+      REAL(KIND = 8), INTENT(IN) :: time
       gamma = 1.4d0
       long = 1.d0
       x0 = long * 0.5d0
@@ -47,18 +48,17 @@ CONTAINS
       ul = 0.d0
       ur = 0.d0
 
-      un(:, 1) = rho_anal(rr)
-      un(:, 2) = mt_anal(1, rr)
-      un(:, 3) = E_anal(rr)
+      un(:, 1) = rho_anal(time, rr)
+      un(:, 2) = mt_anal(1, time, rr)
+      un(:, 3) = E_anal(time, rr)
       CALL set_gamma_for_riemann_solver(gamma)
    END SUBROUTINE init
 
-   FUNCTION rho_anal(rr) RESULT(vv)
-      USE input_data
+   FUNCTION rho_anal(time, rr) RESULT(vv)
       IMPLICIT NONE
       REAL(KIND = 8), DIMENSION(:, :), INTENT(IN) :: rr
+      REAL(KIND = 8), INTENT(IN) :: time
       REAL(KIND = 8), DIMENSION(SIZE(rr, 2)) :: vv
-      REAL(KIND = 8) :: xi, rhostarL, rhostarR, vstar, pstar, lambda1, lambda3
       INTEGER :: n
       IF (SIZE(vv)==0) RETURN
       DO n = 1, SIZE(vv)
@@ -70,12 +70,11 @@ CONTAINS
       END DO
    END FUNCTION rho_anal
 
-   FUNCTION press_anal(rr) RESULT(vv)
+   FUNCTION press_anal(time, rr) RESULT(vv)
       IMPLICIT NONE
       REAL(KIND = 8), DIMENSION(:, :), INTENT(IN) :: rr
+      REAL(KIND = 8), INTENT(IN) :: time
       REAL(KIND = 8), DIMENSION(SIZE(rr, 2)) :: vv
-      REAL(KIND = 8) :: xi, rhostarL, rhostarR, vstar, pstar, lambda1, lambda3
-      INTEGER :: n
       IF (SIZE(vv)==0) RETURN
       DO n = 1, SIZE(vv)
          IF (rr(1, n)<x0) THEN
@@ -86,13 +85,12 @@ CONTAINS
       END DO
    END FUNCTION press_anal
 
-   FUNCTION vit_anal(comp, rr) RESULT(vv)
+   FUNCTION vit_anal(comp, time, rr) RESULT(vv)
       IMPLICIT NONE
       INTEGER, INTENT(IN) :: comp
       REAL(KIND = 8), DIMENSION(:, :), INTENT(IN) :: rr
+      REAL(KIND = 8), INTENT(IN) :: time
       REAL(KIND = 8), DIMENSION(SIZE(rr, 2)) :: vv
-      REAL(KIND = 8) :: xi, rhostarL, rhostarR, vstar, pstar, lambda1, lambda3
-      INTEGER :: n
       IF (SIZE(vv)==0) RETURN
       DO n = 1, SIZE(vv)
          IF (rr(1, n)<x0) THEN
@@ -103,42 +101,38 @@ CONTAINS
       END DO
    END FUNCTION vit_anal
 
-   FUNCTION E_anal(rr) RESULT(vv)
+   FUNCTION E_anal(time, rr) RESULT(vv)
       IMPLICIT NONE
       REAL(KIND = 8), DIMENSION(:, :), INTENT(IN) :: rr
+      REAL(KIND = 8), INTENT(IN) :: time
       REAL(KIND = 8), DIMENSION(SIZE(rr, 2)) :: vv
       vv = press_anal(rr) / (gamma - 1.d0) &
            + rho_anal(rr) * (vit_anal(1, rr)**2) / 2
    END FUNCTION E_anal
 
-   FUNCTION mt_anal(comp, rr) RESULT(vv)
+   FUNCTION mt_anal(comp, time, rr) RESULT(vv)
       IMPLICIT NONE
       INTEGER, INTENT(IN) :: comp
       REAL(KIND = 8), DIMENSION(:, :), INTENT(IN) :: rr
+      REAL(KIND = 8), INTENT(IN) :: time
       REAL(KIND = 8), DIMENSION(SIZE(rr, 2)) :: vv
-      vv = rho_anal(rr) * vit_anal(comp, rr)
+    vv = rho_anal(rr)*vit_anal(comp,rr)
    END FUNCTION mt_anal
 
-   FUNCTION pressure(un) RESULT(vv)
-      IMPLICIT NONE
-      REAL(KIND = 8), DIMENSION(:, :), INTENT(IN) :: un
-      REAL(KIND = 8), DIMENSION(SIZE(un, 1)) :: vv
-      vv = (un(:, 3) - 0.5d0 * (un(:, 2)**2) / un(:, 1)) * (gamma - 1)
-   END FUNCTION pressure
 
-   FUNCTION sol_anal(comp, rr, time) RESULT(vv)
+   FUNCTION sol_anal(comp, time, rr) RESULT(vv)
       IMPLICIT NONE
       INTEGER, INTENT(IN) :: comp
       REAL(KIND = 8), DIMENSION(:, :), INTENT(IN) :: rr
+      REAL(KIND = 8), INTENT(IN) :: time
       REAL(KIND = 8), DIMENSION(SIZE(rr, 2)) :: vv
-      REAL(KIND = 8) :: time
       SELECT CASE(comp)
       CASE(1)
-         vv = rho_anal(rr)
+         vv = rho_anal(time, rr)
       CASE(2)
-         vv = mt_anal(1, rr)
+         vv = mt_anal(1, time, rr)
       CASE(3)
-         vv = E_anal(rr)
+         vv = E_anal(time, rr)
       CASE DEFAULT
          WRITE(*, *) ' BUG in sol_anal'
          STOP
