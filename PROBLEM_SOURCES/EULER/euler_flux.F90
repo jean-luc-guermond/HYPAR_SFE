1bd88b7c65af1aa5f557dab1d5a03246ed9b27d7
mcreff <melvin.creff@gmail.com>
2025-05-02 11:10:47 -0500

    added dij

--- PROBLEM_SOURCES/EULER/euler_flux.F90
+++ PROBLEM_SOURCES/EULER/euler_flux.F90
@@ -1,34 +1,38 @@
 MODULE euler_flux
-  USE space_dim
-  USE eos !<===FIX ME
-  PUBLIC :: flux
+   USE space_dim
+   USE eos !<===FIX ME
+   PUBLIC :: flux
 CONTAINS
-    FUNCTION flux(comp,un) RESULT(vv)
-    IMPLICIT NONE
-    REAL(KIND=8), DIMENSION(:,:),       INTENT(IN) :: un
-    INTEGER,                            INTENT(IN) :: comp
-    REAL(KIND=8), DIMENSION(SIZE(un,1),k_dim)      :: vv
-    REAL(KIND=8), DIMENSION(SIZE(un,1))            :: H, u
-    INTEGER :: k
-    SELECT CASE(comp)
-    CASE(1)
-       DO k = 1, k_dim
-          vv(:,k) = un(:,k+1)
-       END DO
-    CASE(2:k_dim+1)
-       u = un(:,comp)/un(:,1)
-       DO k = 1, k_dim
-          vv(:,k) = un(:,k+1)*u
-       END DO
-       vv(:,comp - 1) = vv(:,comp-1) + pressure(un)
-    CASE(k_dim+2)
-       H = pressure(un) + un(:,comp)
-       DO k = 1, k_dim
-          vv(:,k) = (un(:,k+1)/un(:,1))*H
-       END DO
-    CASE DEFAULT
-       WRITE(*,*) ' BUG in flux'
-       STOP
-    END SELECT
-  END FUNCTION flux
+   FUNCTION flux(comp, un) RESULT(vv)
+      IMPLICIT NONE
+      REAL(KIND = 8), DIMENSION(:, :), INTENT(IN) :: un
+      INTEGER, INTENT(IN) :: comp
+      REAL(KIND = 8), DIMENSION(SIZE(un, 1), k_dim) :: vv
+      REAL(KIND = 8), DIMENSION(SIZE(un, 1)) :: H, u, ie
+      INTEGER :: k
+      SELECT CASE(comp)
+      CASE(1)
+         DO k = 1, k_dim
+            vv(:, k) = un(:, k + 1)
+         END DO
+      CASE(2:k_dim + 1)
+         u = un(:, comp) / un(:, 1)
+         DO k = 1, k_dim
+            vv(:, k) = un(:, k + 1) * u
+         END DO
+         ie = un(:, k_dim + 2) / un(:, 1)
+         DO k = 1, k_dim
+            ie = ie - 0.5d0 * (un(:, k + 1) / un(:, 1))**2
+         END DO
+         vv(:, comp - 1) = vv(:, comp - 1) + pressure(un(:, 1), ie)
+      CASE(k_dim + 2)
+         H = pressure(un) + un(:, comp)
+         DO k = 1, k_dim
+            vv(:, k) = (un(:, k + 1) / un(:, 1)) * H
+         END DO
+      CASE DEFAULT
+         WRITE(*, *) ' BUG in flux'
+         STOP
+      END SELECT
+   END FUNCTION flux
 END MODULE euler_flux
