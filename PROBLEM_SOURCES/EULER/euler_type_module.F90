a0d1eca18b2bbf6dfe22ddce7e8b0354000e84d5
Jean-Luc Guermond <guermond@math.tamu.edu>
2025-05-06 14:38:52 -0500

    jlg

--- PROBLEM_SOURCES/EULER/euler_type_module.F90
+++ PROBLEM_SOURCES/EULER/euler_type_module.F90
@@ -343,7 +343,7 @@ CONTAINS
 
       CALL MatAssemblyBegin(this%matrices%dij, MAT_FINAL_ASSEMBLY, ierr)
       CALL MatAssemblyEnd  (this%matrices%dij, MAT_FINAL_ASSEMBLY, ierr)
-      CALL periodic_matrix_petsc(this%per, this%LA, this%matrices%dij)
+      !CALL periodic_matrix_petsc(this%per, this%LA, this%matrices%dij)
 
    END SUBROUTINE compute_dij
 
