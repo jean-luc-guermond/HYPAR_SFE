168d5a640c96d817cca3c8919b4dc05f1cb13d30
Jean-Luc Guermond <guermond@math.tamu.edu>
2026-03-20 18:11:03 -0500

    jlg

--- PROBLEM_SOURCES/EULER/euler_bc_arrays.F90
+++ PROBLEM_SOURCES/EULER/euler_bc_arrays.F90
@@ -40,59 +40,59 @@ CONTAINS
 
 
     !===Normal at vertices
-    SELECT CASE(k_dim)
-    CASE(2)
-       ALLOCATE(this%surf_normal_vtx(k_dim,mesh%nps))
-       ALLOCATE(this%DIR_normal_vtx(k_dim,SIZE(DIR_js_D)))
-       surf_normal_vtx = 0.d0
-       normal_vtx = 0.d0
-       DO ms = 1, mesh%mes
-          DO ns = 1, mesh%gauss%n_ws
-             js = mesh%iis(ns,ms)
-             normal_vtx(:,js) = normal_vtx(:,js) +  mesh%gauss%rnorms_v(:,ns,ms)
-          END DO
-          !IF (MINVAL(ABS(mesh%sides(ms) - inputs%udotn_zero_list)).NE.0) CYCLE
-          IF (MINVAL(ABS(mesh%sides(ms) - this%udotn_bc%list_sides)).NE.0) CYCLE
-          DO ns = 1, mesh%gauss%n_ws
-             js = mesh%iis(ns,ms)
-             this%surf_normal_vtx(:,js) = this%surf_normal_vtx(:,js) + mesh%gauss%rnorms_v(:,ns,ms)
-          END DO
-       END DO
-       DO ns = 1, mesh%nps
-          this%surf_normal_vtx(:,ns) = this%surf_normal_vtx(:,ns)/SQRT(SUM(this%surf_normal_vtx(:,ns)**2))
-          normal_vtx(:,ns)      = normal_vtx(:,ns)/SQRT(SUM(normal_vtx(:,ns)**2))
-       END DO
-
-       dir = .FALSE.
-       dir(this%udotn_bc%list_sides) = .TRUE.
-       CALL dirichlet_nodes(mesh%iis, mesh%sides, Dir, surf_udotn_js_D)
-       ALLOCATE(udotn_js_D(SIZE(surf_udotn_js_D)))
-       udotn_js_D = mesh%j_s(surf_udotn_js_D)
-
-       virgin = .TRUE.
-       virgin(DIR_js_D) = .FALSE.
-       n = 0
-       DO ns = 1, mesh%nps
-          IF (virgin(ns)) CYCLE
-          n = n+1
-          DIR_js_D(n) = mesh%j_s(ns)
-          DIR_normal_vtx(:,n) = normal_vtx(:,ns)
-       END DO
-       !===Check normal vector
-       ALLOCATE(stuff(k_dim,mesh%np))
-       stuff = 0.d0
-       stuff(1,DIR_js_D)  = DIR_normal_vtx(1,:)
-       stuff(2,DIR_js_D)  = DIR_normal_vtx(2,:)
-       CALL plot_arrow_label(mesh%jj, mesh%rr, stuff, 'normal.plt')
-       DEALLOCATE(stuff)
-    CASE(1)
-       ALLOCATE(udotn_js_D(0))
-       ALLOCATE(surf_normal_vtx(1,2))
-       surf_normal_vtx(1,1) =-1.d0
-       surf_normal_vtx(1,2) = 1.d0
-       normal_vtx(1,1) =-1.d0
-       normal_vtx(1,2) = 1.d0
-    END SELECT
+!!$    SELECT CASE(k_dim)
+!!$    CASE(2)
+!!$       ALLOCATE(this%surf_normal_vtx(k_dim,mesh%nps))
+!!$       ALLOCATE(this%DIR_normal_vtx(k_dim,SIZE(DIR_js_D)))
+!!$       surf_normal_vtx = 0.d0
+!!$       normal_vtx = 0.d0
+!!$       DO ms = 1, mesh%mes
+!!$          DO ns = 1, mesh%gauss%n_ws
+!!$             js = mesh%iis(ns,ms)
+!!$             normal_vtx(:,js) = normal_vtx(:,js) +  mesh%gauss%rnorms_v(:,ns,ms)
+!!$          END DO
+!!$          !IF (MINVAL(ABS(mesh%sides(ms) - inputs%udotn_zero_list)).NE.0) CYCLE
+!!$          IF (MINVAL(ABS(mesh%sides(ms) - this%udotn_bc%list_sides)).NE.0) CYCLE
+!!$          DO ns = 1, mesh%gauss%n_ws
+!!$             js = mesh%iis(ns,ms)
+!!$             this%surf_normal_vtx(:,js) = this%surf_normal_vtx(:,js) + mesh%gauss%rnorms_v(:,ns,ms)
+!!$          END DO
+!!$       END DO
+!!$       DO ns = 1, mesh%nps
+!!$          this%surf_normal_vtx(:,ns) = this%surf_normal_vtx(:,ns)/SQRT(SUM(this%surf_normal_vtx(:,ns)**2))
+!!$          normal_vtx(:,ns)      = normal_vtx(:,ns)/SQRT(SUM(normal_vtx(:,ns)**2))
+!!$       END DO
+!!$
+!!$       dir = .FALSE.
+!!$       dir(this%udotn_bc%list_sides) = .TRUE.
+!!$       CALL dirichlet_nodes(mesh%iis, mesh%sides, Dir, surf_udotn_js_D)
+!!$       ALLOCATE(udotn_js_D(SIZE(surf_udotn_js_D)))
+!!$       udotn_js_D = mesh%j_s(surf_udotn_js_D)
+!!$
+!!$       virgin = .TRUE.
+!!$       virgin(DIR_js_D) = .FALSE.
+!!$       n = 0
+!!$       DO ns = 1, mesh%nps
+!!$          IF (virgin(ns)) CYCLE
+!!$          n = n+1
+!!$          DIR_js_D(n) = mesh%j_s(ns)
+!!$          DIR_normal_vtx(:,n) = normal_vtx(:,ns)
+!!$       END DO
+!!$       !===Check normal vector
+!!$       ALLOCATE(stuff(k_dim,mesh%np))
+!!$       stuff = 0.d0
+!!$       stuff(1,DIR_js_D)  = DIR_normal_vtx(1,:)
+!!$       stuff(2,DIR_js_D)  = DIR_normal_vtx(2,:)
+!!$       CALL plot_arrow_label(mesh%jj, mesh%rr, stuff, 'normal.plt')
+!!$       DEALLOCATE(stuff)
+!!$    CASE(1)
+!!$       ALLOCATE(udotn_js_D(0))
+!!$       ALLOCATE(surf_normal_vtx(1,2))
+!!$       surf_normal_vtx(1,1) =-1.d0
+!!$       surf_normal_vtx(1,2) = 1.d0
+!!$       normal_vtx(1,1) =-1.d0
+!!$       normal_vtx(1,2) = 1.d0
+!!$    END SELECT
 
   END SUBROUTINE construct_euler_bc
 
