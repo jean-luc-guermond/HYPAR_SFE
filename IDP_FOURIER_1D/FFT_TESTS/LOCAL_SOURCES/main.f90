38463fc9ba45f4f8c5760e4d5ef1750dc05fd7d5
Jean-Luc Guermond <guermond@math.tamu.edu>
2026-03-23 20:21:30 -0500

    jlg

--- IDP_FOURIER_1D/FFT_TESTS/LOCAL_SOURCES/main.f90
+++ IDP_FOURIER_1D/FFT_TESTS/LOCAL_SOURCES/main.f90
@@ -1,16 +1,22 @@
 PROGRAM test
   USE fft_1d
   IMPLICIT NONE
-  REAL(KIND=8), PARAMETER ::pi=ACOS(-1.d0)
-  INTEGER, PARAMETER :: Nmaxc = 20
+  REAL(KIND=8), PARAMETER ::pi=ACOS(-1.d0), Length=2.d0
+  INTEGER, PARAMETER :: Nmaxc = 20, k1=5, k2=19
+  REAL(KIND=8), DIMENSION(Nmaxc) :: ar, ai
   REAL(KIND=8), DIMENSION(2*Nmaxc-1) :: r_u, r_out
   REAL(KIND=8),  DIMENSION(Nmaxc,2)  :: cs_u
-  REAL(KIND=8) :: theta
-  INTEGER :: i
-  
+  REAL(KIND=8) :: theta, h,  x
+  INTEGER :: i, k
+
+  CALL RANDOM_NUMBER(ar)
+  CALL RANDOM_NUMBER(ai)
+  ai(1) = 0
   DO i = 1, SIZE(r_u)
      theta =  2*pi*(i-1)/(2*Nmaxc-1)
-     r_u(i)  = 3 + COS(5*theta) + 0.5*SIN(19*theta) 
+     DO k = 1, Nmaxc 
+        r_u(i)  = ar(k)*COS((k-1)*theta) + ai(k)*SIN((k-1)*theta)
+     END DO
   END DO
   CALL real_to_fourier(r_u,cs_u)
 
@@ -20,6 +26,30 @@ PROGRAM test
 
   CALL fourier_to_real(cs_u,r_out)
 
-  WRITE(*,*) 'ERROR', SUM(ABS(r_u-r_out))/SUM(ABS(r_u))
-  
+  WRITE(*,*) 'ERROR FFT', SUM(ABS(r_u-r_out))/SUM(ABS(r_u))
+
+  h = Length/(2*Nmaxc-1)
+  DO i = 1, SIZE(r_u)
+     x = (i-1)*h
+     theta =  2*pi*x/Length
+     DO k = 1, Nmaxc 
+        r_u(i)  = ar(k)*COS((k-1)*theta) + ai(k)*SIN((k-1)*theta)
+     END DO
+  END DO
+
+  CALL real_derivative(r_u,r_out,Length)
+
+  DO i = 1, SIZE(r_u)
+     x = (i-1)*h
+     theta =  2*pi*x/Length
+     DO k = 1, Nmaxc 
+        r_u(i)  = -ar(k)*(k-1)*SIN((k-1)*theta) + ai(k)*(k-1)*COS((k-1)*theta)
+     END DO
+  END DO
+  r_u = r_u/length
+
+  WRITE(*,*) 'ERROR on Derivative', SUM(ABS(r_u-r_out))/SUM(ABS(r_u))
+  DO i = 1, SIZE(r_u)
+     write(*,*) r_u(i), r_out(i)
+  END DO
 END PROGRAM test
--- IDP_FOURIER_1D/SOURCES/fft.f90
+++ IDP_FOURIER_1D/SOURCES/fft.f90
@@ -1,4 +1,5 @@
 MODULE fft_1D
+  REAL(KIND=8), PRIVATE, PARAMETER :: pi=ACOS(-1.d0)
 CONTAINS
   SUBROUTINE real_to_fourier(r_in,cs_out)
     IMPLICIT NONE
@@ -9,44 +10,59 @@ CONTAINS
     REAL(KIND=8) :: scaling
     INTEGER(kind=8) :: plan
     INTEGER :: N, i
-    
     N = SIZE(r_in)
     scaling = 2.d0/N
-
     CALL dfftw_plan_dft_r2c_1d(plan,N,r_in,cout,FFTW_ESTIMATE)
     CALL dfftw_execute_dft_r2c(plan, r_in, cout)
     CALL dfftw_destroy_plan(plan)
-
-    write(*,*) 'TEST', (N+1)/2, N/2+1 
     DO i = 1, (N+1)/2
        cs_out(i,1) = REAL (cout(i),KIND=8)
        cs_out(i,2) = -AIMAG (cout(i))
     END DO
     cs_out = scaling*cs_out 
     cs_out (1,:) =  cs_out(1,:)/2 !===mode 0
-  
   END SUBROUTINE real_to_fourier
 
   SUBROUTINE fourier_to_real(cs_in,r_out)
     IMPLICIT NONE
     INCLUDE 'fftw3.f'
-    REAL(KIND=8),    DIMENSION(:,:)            :: cs_in !===Complex size N
+    REAL(KIND=8),    DIMENSION(:,:)            :: cs_in !===(N,2)
     COMPLEX(KIND=8), DIMENSION(SIZE(cs_in,1))  :: cin !===Complex size N
     REAL(KIND=8), DIMENSION(2*SIZE(cs_in,1)-1) :: r_out !===size 2N-1
     REAL(KIND=8) :: scaling
     INTEGER(kind=8) :: plan
     INTEGER :: N
-    
     N = 2*SIZE(cs_in,1)-1
     scaling = 2.d0/N
-
     cin = 0.5d0*CMPLX(cs_in(:,1),-cs_in(:,2),KIND=8)
     cin(1) = 2*cin(1)
-    
     CALL dfftw_plan_dft_c2r_1d(plan,N,cin,r_out,FFTW_ESTIMATE)
     CALL dfftw_execute_dft_c2r(plan, cin, r_out)
     CALL dfftw_destroy_plan(plan)
-
-    write(*,*) 'TEST', (N+1)/2, N/2+1
   END SUBROUTINE fourier_to_real
+  
+  SUBROUTINE fourier_derivative(cs_in,cs_out,Length)
+    IMPLICIT NONE
+    REAL(KIND=8),    DIMENSION(:,:) :: cs_in 
+    REAL(KIND=8),    DIMENSION(SIZE(cs_in,1),SIZE(cs_in,2)) :: cs_out
+    REAL(KIND=8) :: Length
+    INTEGER :: i
+    DO i = 1, SIZE(cs_in,1)
+       cs_out(i,1) =  (i-1)*cs_in(i,2)
+       cs_out(i,2) = -(i-1)*cs_in(i,1)
+    END DO
+    cs_out = cs_out/Length
+  END SUBROUTINE fourier_derivative
+
+  SUBROUTINE real_derivative(r_in,r_out,Length)
+    IMPLICIT NONE
+    INCLUDE 'fftw3.f'
+    REAL(KIND=8), DIMENSION(:) :: r_in !===size 2N-1
+    REAL(KIND=8), DIMENSION(SIZE(r_in)) :: r_out
+    REAL(KIND=8), DIMENSION(SIZE(r_in)/2+1,2) :: cs_in, cs_out
+    REAL(KIND=8) :: Length
+    CALL real_to_fourier(r_in,cs_in)
+    CALL fourier_derivative(cs_in,cs_out,Length)
+    CALL fourier_to_real(cs_out,r_out)
+  END SUBROUTINE real_derivative
 END MODULE fft_1D
