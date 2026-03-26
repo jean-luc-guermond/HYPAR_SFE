07838360c5857d99907f73759605eada61b9bcda
Jean-Luc Guermond <guermond@math.tamu.edu>
2026-03-26 11:49:04 -0500

    jlg

---
+++ IDP_FOURIER_1D/PROJECT_TEMPLATES/BURGERS/LOCAL_SOURCES/main.f90
@@ -0,0 +1,40 @@
+PROGRAM linear_transport
+  USE setup_module
+  USE start_setup_MODULE
+  USE my_util
+  USE fft_1D
+  IMPLICIT NONE
+  INTEGER :: n
+  REAL(KIND = 8) :: tps
+  
+  CALL start_setup
+
+  nl_scalar_cons%un = exact_sol_F(fourier_param, nl_scalar_cons%time,exact_sol_step_R) 
+  CALL fourier_param%plot_1d(exact_sol_step_R(fourier_param, nl_scalar_cons%time), 'u_init.plt')
+
+  tps = user_time()
+  n = 0
+  DO WHILE(nl_scalar_cons%time < setup_data%final_time)
+     CALL nl_scalar_cons%update(fourier_param)
+     n = n + 1
+     !write(*, *) n, nl_scalar_cons%time, nl_scalar_cons%dt
+  END DO
+  tps = user_time() - tps
+  CALL errors(nl_scalar_cons)
+CONTAINS
+  SUBROUTINE errors(nl_scalar_cons)
+    USE nl_scalar_cons_module
+    USE fft_1D
+    IMPLICIT NONE
+    type(nl_scalar_cons_type) :: nl_scalar_cons
+    REAL(KIND = 8), DIMENSION(fourier_param%Nmax_real) :: r_un
+    REAL(KIND=8) :: error, norm
+    
+    CALL fourier_to_real(nl_scalar_cons%un,r_un)
+    CALL fourier_param%plot_1d(r_un, 'u.plt')
+    CALL fourier_param%plot_1d(exact_sol_step_R(fourier_param, nl_scalar_cons%time), 'ue.plt')
+    error = SUM(ABS(r_un-exact_sol_step_R(fourier_param, nl_scalar_cons%time)))
+    norm = SUM(ABS(r_un))
+    WRITE(*,*) ' Error relative L1-norm', error/norm 
+  END SUBROUTINE errors
+END PROGRAM linear_transport
---
+++ IDP_FOURIER_1D/PROJECT_TEMPLATES/BURGERS/LOCAL_SOURCES/setup.f90
@@ -0,0 +1,75 @@
+MODULE setup_module
+  !TYPE setup_type
+  ! CONTAINS
+  !    PROCEDURE, PUBLIC, NOPASS :: flux=>linear_flux
+  !   PROCEDURE, PUBLIC, NOPASS :: lambda_max=>linear_lambda_max
+  !END TYPE setup_type
+CONTAINS
+
+  FUNCTION flux(u) RESULT(vv)
+    IMPLICIT NONE
+    REAL(KIND = 8), DIMENSION(:), INTENT(IN) :: u
+    REAL(KIND = 8), DIMENSION(SIZE(u)) :: vv
+    vv = u**2/2
+  END FUNCTION  flux
+
+  FUNCTION lambda_max(ul,ur) RESULT(vv)
+    IMPLICIT NONE
+    REAL(KIND = 8), INTENT(IN) :: ul, ur
+    REAL(KIND = 8) :: vv
+    IF (ul>ur) THEN
+       vv = abs(ul+ur)/2
+    ELSE
+       vv = max(abs(ul),abs(ur))
+    END IF
+  END FUNCTION lambda_max
+
+  FUNCTION exact_sol_step_R(fourier_param,time) RESULT(vv)
+    USE fourier_param_module
+    IMPLICIT NONE
+    TYPE(fourier_param_type) :: fourier_param
+    REAL(KIND=8), DIMENSION(fourier_param%Nmax_real) :: vv
+    REAL(KIND=8) :: time
+    REAL(KIND=8), PARAMETER :: pi=ACOS(-1.d0)
+    vv = SIN(2*pi*Fourier_param%rr/Fourier_param%Length)
+  END FUNCTION exact_sol_step_R
+
+  FUNCTION exact_sol_step_R_1(fourier_param,time) RESULT(vv)
+    USE fourier_param_module
+    IMPLICIT NONE
+    TYPE(fourier_param_type) :: fourier_param
+    REAL(KIND=8), DIMENSION(fourier_param%Nmax_real) :: vv
+    REAL(KIND=8) :: time
+    REAL(KIND=8), PARAMETER :: pi=ACOS(-1.d0)
+    INTEGER :: i
+    DO i = 1, Fourier_param%NMax_real
+       IF (Fourier_param%rr(i)< Fourier_param%Length/2) THEN
+          vv(i) = Fourier_param%rr(i)/(1+time)
+       ELSE
+          vv(i) = (Fourier_param%rr(i)-Fourier_param%Length)/(1+time)
+       END IF
+    END DO
+  END FUNCTION exact_sol_step_R_1
+
+  FUNCTION exact_sol_F(fourier_param,time,exact_sol_R) RESULT(cs_v)
+    USE fourier_param_module
+    USE fft_1D
+    IMPLICIT NONE
+    TYPE(fourier_param_type) :: fourier_param
+    REAL(KIND=8), DIMENSION(fourier_param%Nmax,2) :: cs_v
+    REAL(KIND=8), DIMENSION(fourier_param%Nmax_real) :: r_v
+    REAL(KIND=8) :: time
+    interface
+       function exact_sol_R(fourier_param,time) RESULT(vv)
+         USE fourier_param_module
+         IMPLICIT NONE
+         TYPE(fourier_param_type) :: fourier_param
+         REAL(KIND=8), DIMENSION(fourier_param%Nmax_real) :: vv
+         REAL(KIND=8) :: time
+       END FUNCTION exact_sol_R
+    end interface
+    r_v = exact_sol_R(fourier_param,time)
+    CALL real_to_fourier(r_v,cs_v)
+  END FUNCTION exact_sol_F
+
+END MODULE setup_module
