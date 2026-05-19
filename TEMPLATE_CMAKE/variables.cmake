afe4f8d408fae670e7fc07959ef273b0e704e983
Jean-Luc Guermond <guermond@math.tamu.edu>
2026-05-19 16:40:12 -0500

    jlg

--- TEMPLATE_CMAKE/variables.cmake
+++ TEMPLATE_CMAKE/variables.cmake
@@ -1,6 +1,6 @@
 # Variables for Whistler
 #==== Mandatory for any application ====
-set(HYPAR_SFE_DIR "my_path_to_hypar/HYPAR_SFE")
+set(HYPAR_SFE_DIR "/home/guermond/HYPAR_SFE")
 set(debug_bounds "-Wall -fimplicit-none -fbounds-check")
 set(release_bounds "-O3")
 set(FE_dim "1") #1 or 2
@@ -9,4 +9,4 @@ set(FE_dim "1") #1 or 2
 set(RUN_PRE_PROC "mpirun")
 set(PROC_CALL "-np ")
 set(RUN_POST_PROC "")
-set(PARALLEL_TEST_LEVEL 0) 
+set(PARALLEL_TEST_LEVEL 0)
