b2cf7d23da54afdb82f1cf24601f9cdf40aafa64
Victor <botez@lisn.fr>
2026-05-26 03:47:52 +0200

    vb: first to generalize refine_mesh/parameter for max number of ctest processes

--- COMMON_SOURCES/MESH/2D/refine_mesh.F90
+++ COMMON_SOURCES/MESH/2D/refine_mesh.F90
@@ -14,17 +14,17 @@ CONTAINS
       !===jjs_f(:, :)  nodes of the surface_elements of the output p2 grid
       !===rr_f(:, :)  cartesian coordinates of the nodes of the output p2 grid
       USE def_type_mesh
-      USE my_util, ONLY : error_petsc
+      USE my_util, ONLY : error_petsc, to_str
       IMPLICIT NONE
       TYPE(mesh_type) :: mesh_p1, mesh
       INTEGER, INTENT(IN) :: type_fe
       LOGICAL, DIMENSION(:), ALLOCATABLE :: virgin
-      INTEGER, DIMENSION(:, :), ALLOCATABLE :: j_mid, jjs_mid
+      INTEGER, DIMENSION(:, :), ALLOCATABLE :: j_edge, jjs_edge
       INTEGER :: np, me, mes, nw, nws, kd, n, m, k, l, n_dof, dom_np, ne
       INTEGER :: n1, n2, ms, n_start, n_end, n_loc
       INTEGER :: n_k1, n_k2, m_op_k, kk, i, mm, p_e, p_c
       REAL(KIND = 8), DIMENSION(:), ALLOCATABLE :: r_mid
-      INTEGER :: nb_angle, f_dof, edge_g, edge_l, n_new_start, proc, nb_proc, edges, p, cell_g, cell_l
+      INTEGER :: nb_angle, f_dof, f_dof_edge, f_dof_cell,edge_g, edge_l, n_new_start, proc, nb_proc, edges, p, cell_g, cell_l
       INTEGER :: interface
       LOGICAL :: iso
 
@@ -39,6 +39,8 @@ CONTAINS
       mes = SIZE(mesh_p1%jjs, 2)
       nws = SIZE(mesh_p1%jjs, 1)
       f_dof = type_fe - 1
+      f_dof_edge = type_fe - 1
+      f_dof_cell = (type_fe - 1) * (type_fe - 2) / 2
       nb_proc = SIZE(mesh_p1%domnp)
 
       ALLOCATE(mesh%jjs_int(SIZE(mesh_p1%jjs_int, 1), SIZE(mesh_p1%jjs_int, 2)))
@@ -138,11 +140,10 @@ CONTAINS
       END IF
       
       nb_angle = 0
-      ALLOCATE(virgin(mesh_p1%medge), j_mid(nw * f_dof, me), jjs_mid(f_dof, mes), r_mid(kd))
+      ALLOCATE(virgin(mesh_p1%medge), j_edge(nw * f_dof, me), jjs_edge(f_dof, mes), r_mid(kd))
 
       IF (kd == 3) THEN
-         WRITE(*, *) ' CREATE_GRID_Pk: 3D case not programmed yet !'
-         STOP
+         CALL error_petsc('create_iso_grid_distributed: 3D case not programmed yet !')
       END IF
 
       proc = mesh_p1%proc
@@ -150,28 +151,25 @@ CONTAINS
       mesh%proc_np_loc(:,1:SIZE(mesh_P1%proc_np_loc,2)) = mesh_P1%proc_np_loc(:,:)
 
       !===GENERATION OF THE Pk GRID
+
+!=== Copy the existing P1 grid ===!
       mesh%rr(:, 1:dom_np) = mesh_p1%rr(:, 1:dom_np)
       mesh%rr(:, mesh%dom_np + 1:mesh%dom_np + np - dom_np) = mesh_p1%rr(:, dom_np + 1:)
       mesh%jj(1:nw, :) = mesh_p1%jj
       mesh%jj_extra(1:nw, :) = mesh_p1%jj_extra
-!VB useless??
-      ! IF (size(mesh_p1%jjs_extra)/=0) THEN
-      !    mesh%jjs_extra(1:nws, 1:mesh_p1%mes_extra) = mesh_p1%jjs_extra !<== JLG April 16, 2026
-      !    !mesh%jjs_extra(1:nws, :) = mesh_p1%jjs_extra
-      ! END IF
-!VB
+
       IF (size(mesh_p1%rrs_extra)/=0) THEN
          mesh%rrs_extra(:, 1:nw, 1:mesh_p1%mes_extra) = mesh_p1%rrs_extra !<== JLG April 16, 2026
          !mesh%rrs_extra(:, 1:nw, :) = mesh_p1%rrs_extra
       END IF
 
       mesh%isolated_jjs = mesh_p1%isolated_jjs &
-           + (mesh_p1%disedge(proc) - 1) * f_dof + (mesh_p1%discell(proc) - 1) * (f_dof - 1)
+           + (mesh_p1%disedge(proc) - 1) * (f_dof_edge) + (mesh_p1%discell(proc) - 1) * (f_dof_cell)
 
       DO m = 1, mesh%me
          DO n = 1, nw
             IF (mesh_p1%jj(n, m) > mesh_p1%dom_np) THEN
-               n_loc = mesh_p1%jj(n, m) + mesh_p1%medge * f_dof + mesh_p1%me * (f_dof - 1)
+               n_loc = mesh_p1%jj(n, m) + mesh_p1%medge * f_dof_edge + mesh_p1%me * f_dof_cell
                mesh%jj(n, m) = n_loc
             END IF
          END DO
@@ -183,7 +181,7 @@ CONTAINS
          DO n = 1, nw
             p = mesh_p1%get_proc(mesh_p1%jj_extra(n, m), 'np')
             mesh%jj_extra(n, m) = mesh_p1%jj_extra(n, m) &
-                 + (mesh_p1%disedge(p) - 1) * f_dof + (mesh_p1%discell(p) - 1) * (f_dof - 1)
+                 + (mesh_p1%disedge(p) - 1) * f_dof_edge + (mesh_p1%discell(p) - 1) * f_dof_cell
          END DO
       END DO
 
@@ -191,10 +189,13 @@ CONTAINS
          DO n = 1, nws
             p = mesh_p1%get_proc(mesh_p1%jjs_extra(n, m), 'np')
             mesh%jjs_extra(n, m) = mesh_p1%jjs_extra(n, m) &
-                 + (mesh_p1%disedge(p) - 1) * f_dof + (mesh_p1%discell(p) - 1) * (f_dof - 1)
+                 + (mesh_p1%disedge(p) - 1) * f_dof_edge + (mesh_p1%discell(p) - 1) * f_dof_cell
          END DO
       END DO
+!=== End copy the existing P1 grid ===!
 
+
+!=== Loop on inner edges, owned by this proc ===!
       virgin = .TRUE.
       n_dof = 0
       DO m = 1, me !===loop on the elements
@@ -204,7 +205,7 @@ CONTAINS
 
             IF (edge_l <= 0) CYCLE
 
-            n_new_start = (edge_l - 1) * f_dof + mesh_p1%dom_np
+            n_new_start = (edge_l - 1) * f_dof_edge + mesh_p1%dom_np
 
             m_op_k = mesh_p1%neigh(k, m)
             n_k1 = MODULO(k, nw) + 1
@@ -223,8 +224,10 @@ CONTAINS
             iso = .FALSE.
             IF (m_op_k == 0) THEN  !===the side is on the boundary
                DO ms = 1, SIZE(mesh_p1%neighs) + 1
-                  IF (ms == SIZE(mesh_p1%neighs) + 1) WRITE(*, *) &
-                       'BUG in create_iso_grid: cell near boundary isnt in neighs', m_op_k, m
+                  IF (ms == SIZE(mesh_p1%neighs) + 1) THEN
+                     CALL error_petsc("BUG in create_iso_grid_distributed: cell near boundary isnt in neighs "&
+                     //to_str(m_op_k)//', '//to_str(m))
+                  END IF
                   IF (mesh_p1%neighs(ms) == m) EXIT
                END DO
                !               CALL is_on_curved_interface(mesh_p1%sides(ms), iso, interface)
@@ -232,9 +235,9 @@ CONTAINS
             END IF
 
             IF (virgin(edge_l)) THEN !===This side is new
-               DO l = 1, f_dof
+               DO l = 1, f_dof_edge
                   n_dof = n_dof + 1 !===New index created
-                  j_mid((k - 1) * f_dof + l, m) = l + n_new_start
+                  j_edge((k - 1) * f_dof_edge + l, m) = l + n_new_start
                   mesh%rr(:, l + n_new_start) = mesh_p1%rr(:, n_start) &
                        + l * (mesh_p1%rr(:, n_end) - mesh_p1%rr(:, n_start)) / type_fe
                   !                  IF (iso) THEN
@@ -249,18 +252,20 @@ CONTAINS
                      EXIT
                   END IF
                ENDDO
-               DO l = 1, f_dof
-                  j_mid((k - 1) * f_dof + l, m) = j_mid((kk - 1) * f_dof + l, mm) !===New index created
+               DO l = 1, f_dof_edge
+                  j_edge((k - 1) * f_dof_edge + l, m) = j_edge((kk - 1) * f_dof_edge + l, mm) !===New index created
                END DO
             ENDIF
             virgin(edge_l) = .FALSE.
          ENDDO
       ENDDO
 
-      IF (n_dof /= mesh_p1%medge * f_dof) THEN
-         WRITE(*, *) 'BUG in create_iso_grid_distributed, n_dof /= mesh_p1%medge * f_dof'
-         STOP
+      IF (n_dof /= mesh_p1%medge * f_dof_edge) THEN
+         CALL error_petsc('create_iso_grid_distributed: n_dof /= mesh_p1%medge * f_dof_edge (I)')
       END IF
+      
+      
+!=== Loop on outer edges, not owned by this proc ===!
       n_dof = 0
       DO edges = 1, mesh_p1%medges
          edge_g = mesh_p1%jees(edges)
@@ -284,49 +289,50 @@ CONTAINS
          END IF
 
          edge_l = edge_g - mesh_p1%disedge(p) + 1
-         n_new_start = (edges - 1) * f_dof + mesh_p1%me * (f_dof - 1) + mesh_p1%medge * f_dof + mesh_p1%np
+         n_new_start = (edges - 1) * f_dof_edge + mesh_p1%me * f_dof_cell + mesh_p1%medge * f_dof_edge + mesh_p1%np
 
-         DO l = 1, f_dof
+         DO l = 1, f_dof_edge
             n_dof = n_dof + 1 !===New index created
-            j_mid((k - 1) * f_dof + l, m) = l + n_new_start
+            j_edge((k - 1) * f_dof_edge + l, m) = l + n_new_start
             mesh%rr(:, n_new_start + l) = mesh_p1%rr(:, n_start) &
                  + l * (mesh_p1%rr(:, n_end) - mesh_p1%rr(:, n_start)) / type_fe
             
-            IF (p==proc) THEN
-               write(*,*) 'same proc?'
-               stop
-            ELSE IF (n_new_start + l <= mesh%dom_np) THEN
-               write(*,*) "n_new_start + l <= mesh%dom_np ?? "
-               stop
+            IF ((p==proc) .OR. (n_new_start + l <= mesh%dom_np)) THEN
+               CALL error_petsc("BUG in create_iso_grid_distributed: edge on proc boundary should not be owned by this proc: "&
+               //to_str(p)//" == "//to_str(proc)//" OR dom_np = "//to_str(mesh%dom_np)//&
+               " >= n_new_start + l = "//to_str(n_new_start + l))
             END IF
             mesh%proc_np_loc(1, n_new_start + l - mesh%dom_np) = p
-            mesh%proc_np_loc(2, n_new_start + l - mesh%dom_np) = l + (edge_l - 1) * f_dof + mesh_p1%domnp(p)
+            mesh%proc_np_loc(2, n_new_start + l - mesh%dom_np) = l + (edge_l - 1) * f_dof_edge + mesh_p1%domnp(p)
          END DO
 
       END DO
 
-      IF (n_dof /= mesh_p1%medges * f_dof) THEN
-         WRITE(*, *) 'BUG in create_iso_grid_distributed, n_dof /= mesh_p1%medge * f_dof'
-         STOP
+      IF (n_dof /= mesh_p1%medges * f_dof_edge) THEN
+         CALL error_petsc('create_iso_grid_distributed: n_dof /= mesh_p1%medges * f_dof_edge (II)')
       END IF
       n_dof = 0
-      !===connectivity array for iso grid
+      
+      !===connectivity array for iso grid deduced from j_edge, + additional point in P3
       DO m = 1, me
-         DO n = 1, f_dof * nw
-            mesh%jj(nw + n, m) = j_mid(n, m)
+         DO n = 1, f_dof_edge * ne
+            mesh%jj(nw + n, m) = j_edge(n, m)
          END DO
+         ! n_loc = f_dof_edge * ne + 3 !offset by summits and edge points
+         ! DO n = 1, f_dof_cell !TODO AFTER VALIDATING REFINEMENT ORDER 3
          IF (type_fe==3) THEN
             n_dof = n_dof + 1
-            n_new_start = m + mesh_p1%dom_np + mesh_p1%medge * 2
+            n_new_start = m + mesh_p1%dom_np + mesh_p1%medge * f_dof_edge
             mesh%jj(10, m) = n_new_start
+            ! mesh%jj(n_loc+n, m) = n_new_start
             mesh%rr(:, n_new_start) = &
-                 (mesh_p1%rr(:, mesh_p1%jj(1, m)) + mesh_p1%rr(:, mesh_p1%jj(2, m)) + mesh_p1%rr(:, mesh_p1%jj(3, m))) / 3
+                 (mesh_p1%rr(:, mesh_p1%jj(1, m)) + mesh_p1%rr(:, mesh_p1%jj(2, m)) + mesh_p1%rr(:, mesh_p1%jj(3, m))) / type_fe
          END IF
+         ! END DO
       END DO
 
-      IF (type_fe == 3 .and. n_dof /= mesh_p1%me) THEN
-         WRITE(*, *) 'BUG in create_iso_grid_distributed, type_fe == 3 .and. n_dof /= mesh_p1%me'
-         STOP
+      IF (type_fe == 3 .AND. n_dof /= mesh_p1%me) THEN
+         CALL error_petsc('BUG in create_iso_grid_distributed, type_fe == 3 .and. n_dof /= mesh_p1%me')
       END IF
 
       !==connectivity array the surface elements of the iso grid
@@ -339,7 +345,7 @@ CONTAINS
             END IF
          ENDDO
          DO l = 1, f_dof
-            jjs_mid(l, ms) = j_mid((kk - 1) * f_dof + l, m) !===New index created
+            jjs_edge(l, ms) = j_edge((kk - 1) * f_dof + l, m) !===New index created
          END DO
       ENDDO
       mesh%jjs(1:nws, :) = mesh_p1%jjs
@@ -351,9 +357,9 @@ CONTAINS
          END DO
       END DO
 
-      mesh%jjs(nws + 1:, :) = jjs_mid
+      mesh%jjs(nws + 1:, :) = jjs_edge
 
-      DEALLOCATE(virgin, j_mid, jjs_mid, r_mid)
+      DEALLOCATE(virgin, j_edge, jjs_edge, r_mid)
       !===new vertices on extra cells
       DO m = 1, mesh%mextra
          DO k = 1, nw !===loop on the nodes (sides) of the element
@@ -444,14 +450,29 @@ CONTAINS
       TYPE(mesh_type) :: mesh_p1, mesh
       INTEGER :: np, me, mes, nw, nws, kd, n, m, k, n_dof, dom_np, e_g, mextra
       INTEGER :: n1, n2, ms, neigh, k_neigh, n_kneigh1, n_kneigh2, swap, n_loc
-      INTEGER :: i, p_c, m_new, e_k, p_j
+      INTEGER :: i, p_c, m_new, e_k, p_j, ne, refine_order
       INTEGER, DIMENSION(3) :: edges_g, edges_l, p_es, sub_cell
       INTEGER, DIMENSION(2) :: n_ks
+      INTEGER, DIMENSION(:,:), ALLOCATABLE :: jj_HL, jjs_HL
       INTEGER :: proc, nb_proc, p, cell_g, cell_l
       INTEGER :: m1, m2, interface, m_center, tab1, tab2, mes_int
       LOGICAL :: iso
       type(mesh_type) :: mesh_p2
 
+
+      refine_order = 2
+      ne = 3
+
+      ALLOCATE(jj_HL(refine_order**2, ne))
+      ALLOCATE(jjs_HL(refine_order, ne-1))
+
+      jj_HL(1, :) = [4, 5, 6]
+      jj_HL(2, :) = [1, 5, 6]
+      jj_HL(3, :) = [2, 4, 6]
+      jj_HL(4, :) = [3, 4, 5]
+
+      jjs_HL(1, :) = [1, 3]
+      jjs_HL(2, :) = [2, 3]
 
       IF (mesh_p1%me == 0) RETURN
 
@@ -466,12 +487,13 @@ CONTAINS
       nws = SIZE(mesh_p1%jjs, 1)
       nb_proc = SIZE(mesh_p1%domnp)
 
-      mesh%me = 4 * mesh_p1%me
-      mesh%mes = 2 * mesh_p1%mes !---> something with news to take into account
-      mesh%mes_int = 2 * mesh_p1%mes_int
-      mesh%np = mesh_p1%np + mesh_p1%medge + mesh_p1%medges
-      mesh%medge = 2 * mesh_p1%medge + 3 * mesh_p1%me
-      mesh%medges = 2 * mesh_p1%medges
+      mesh%me = refine_order**2 * mesh_p1%me
+      mesh%mes = refine_order * mesh_p1%mes !---> something with news to take into account
+      mesh%mes_int = refine_order * mesh_p1%mes_int
+      mesh%np = mesh_p1%np + (mesh_p1%medge + mesh_p1%medges)*(refine_order - 1) + mesh_p1%me*(refine_order-1)*(refine_order-2)/2
+      mesh%medge = refine_order * mesh_p1%medge + 3*(refine_order-1)*refine_order/2*mesh_p1%me
+      mesh%medges = refine_order * mesh_p1%medges
+
       ALLOCATE(mesh%jj(nw, mesh%me)) !--->done
       ALLOCATE(mesh%jjs(nws, mesh%mes))  !--->done
       ALLOCATE(mesh%rr(kd, mesh%np)) !--->done
@@ -503,7 +525,7 @@ CONTAINS
       ALLOCATE(mesh%isolated_jjs(mesh_p1%nis))
 
       IF (kd == 3) THEN
-         CALL error_petsc('CREATE_GRID_Pk: 3D case not programmed yet !')
+         CALL error_petsc('refinement_iso_grid_distributed: 3D case not programmed yet !')
       END IF
 
       
@@ -511,7 +533,7 @@ CONTAINS
       proc = mesh_p1%get_proc(mesh_p1%loc_to_glob(1), 'np')
 
       !===GENERATION OF THE Pk GRID
-      CALL create_iso_grid_distributed(mesh_p1, mesh_p2, 2)
+      CALL create_iso_grid_distributed(mesh_p1, mesh_p2, refine_order)
       mesh%rr = mesh_p2%rr
 
       ALLOCATE(mesh%proc_np_loc(2, mesh%np-mesh%dom_np))
@@ -519,27 +541,26 @@ CONTAINS
       mesh%isolated_jjs = mesh_p2%isolated_jjs
 
       DO m=1, me
-         m_center = 4 * (m - 1) + 1
-         mesh%jj(:, m_center    ) = mesh_p2%jj([4,5,6], m)
-         mesh%jj(:, m_center + 1) = mesh_p2%jj([1,5,6], m)
-         mesh%jj(:, m_center + 2) = mesh_p2%jj([2,4,6], m)
-         mesh%jj(:, m_center + 3) = mesh_p2%jj([3,4,5], m)
+         m_center = refine_order**2 * (m - 1) + 1
+         DO i=1, refine_order**2
+            mesh%jj(:, m_center + i - 1) = mesh_p2%jj(jj_HL(i, :), m)
+         END DO
       END DO
-
 
 
       n_dof = 0
       DO m = 1, me !===loop on the elements unrefined mesh
          edges_g = mesh_p1%jce(:, m)
          edges_l = edges_g - mesh_p1%disedge(proc) + 1
-         m_center = 4 * (m - 1) + 1
+         m_center = refine_order**2 * (m - 1) + 1
          !===Center cell
          mesh%i_d(m_center) = mesh_p1%i_d(m)
          DO i = 1, nw
             !===Setting up neighbours and edges
             mesh%neigh(i, m_center) = m_center + i
             mesh%i_d(mesh%neigh(i, m_center)) = mesh_p1%i_d(m)
-            mesh%jce(i, m_center) = mesh%disedge(proc) - 1 + 2 * mesh_p1%medge + 3 * (m - 1) + i
+            mesh%jce(i, m_center) = mesh%disedge(proc) - 1 + refine_order * mesh_p1%medge &
+                                    + 3*(refine_order-1)*refine_order/2*(m - 1) + i
          END DO
 
 
@@ -585,9 +606,6 @@ CONTAINS
                e_g = mesh_p1%jce(n_ks(MODULO(e_k, 2) + 1), m)
                IF (e_g < mesh_p1%disedge(proc)) THEN
                   p = mesh_p1%get_proc(e_g, 'medge')
-                  DO ms = 1, mesh_p1%medges
-                     IF (mesh_p1%jees(ms) == e_g) EXIT
-                  END DO
                ELSE
                   p = proc
                END IF
@@ -599,6 +617,9 @@ CONTAINS
                END IF
 
                IF (e_g < mesh_p1%disedge(proc)) THEN
+                  DO ms = 1, mesh_p1%medges
+                     IF (mesh_p1%jees(ms) == e_g) EXIT
+                  END DO
                   IF (k < n_ks(e_k)) THEN
                      mesh%jees(ms) = mesh%jce(e_k + 1, m_new)
                      mesh%jecs(ms) = m_new
@@ -612,8 +633,9 @@ CONTAINS
       END DO
 
       DO ms = 1, mes
-         mesh%jjs(:, ms) = mesh_p2%jjs([1, 3], ms)
-         mesh%jjs(:, ms + mes) = mesh_p2%jjs([2, 3], ms)
+         DO i=1, refine_order
+            mesh%jjs(:, ms + (i - 1) * mes) = mesh_p2%jjs(jjs_HL(i, :), ms)
+         ENDDO
       END DO
 
       !===Surface elements
@@ -700,21 +722,17 @@ CONTAINS
       !         END DO
       !      END DO
       !===In the mean time
-      mesh%mextra = mesh_p1%mextra * 4
+      mesh%mextra = mesh_p1%mextra * refine_order**2
       ALLOCATE(mesh%jj_extra(nw, mesh%mextra)) !---->
       ALLOCATE(mesh%jce_extra(nw, mesh%mextra)) !---->
       ALLOCATE(mesh%jcc_extra(mesh%mextra)) !---->
 
       mextra = 0
       DO m=1, mesh_p1%mextra
-         mextra = mextra + 1
-         mesh%jj_extra(:, mextra    ) = mesh_p2%jj_extra([4,5,6], m)
-         mextra = mextra + 1
-         mesh%jj_extra(:, mextra) = mesh_p2%jj_extra([1,5,6], m)
-         mextra = mextra + 1
-         mesh%jj_extra(:, mextra) = mesh_p2%jj_extra([2,4,6], m)
-         mextra = mextra + 1
-         mesh%jj_extra(:, mextra) = mesh_p2%jj_extra([3,4,5], m)
+         DO i=1, refine_order**2
+            mextra = mextra + 1
+            mesh%jj_extra(:, mextra) = mesh_p2%jj_extra(jj_HL(i, :), m)
+         END DO
       END DO
 
 
@@ -732,9 +750,6 @@ CONTAINS
 
          DO i = 1, 3
             mesh%jce_extra(i, mextra) = mesh%disedge(p_c) - 1 + 2 * mesh_p1%domedge(p_c) + 3 * (cell_l - 1) + i
-            e_g = mesh_p1%jce_extra(i, m)
-            p = mesh_p1%get_proc(e_g, 'medge')
-            ! mesh%jj_extra(i, mextra) = mesh%disp(p) - 1 + mesh_p1%domnp(p) + e_g - mesh_p1%disedge(p) + 1
          END DO
 
          !corner cells
@@ -856,10 +871,14 @@ CONTAINS
       
       mextra = 0
       DO m=1, mesh_p1%mes_extra
-         mextra = mextra + 1
-         mesh%jjs_extra(:, mextra) = mesh_p2%jjs_extra([1, 3], (mextra+1)/2)
-         mextra = mextra + 1
-         mesh%jjs_extra(:, mextra) = mesh_p2%jjs_extra([2, 3], (mextra+1)/2)
+         DO i=1, refine_order
+            mextra = mextra + 1
+            mesh%jjs_extra(:, mextra) = mesh_p2%jjs_extra(jjs_HL(i, :), (mextra+1)/2)
+         END DO
+         ! mextra = mextra + 1
+         ! mesh%jjs_extra(:, mextra) = mesh_p2%jjs_extra(jjs_HL(1, :), (mextra+1)/2)
+         ! mextra = mextra + 1
+         ! mesh%jjs_extra(:, mextra) = mesh_p2%jjs_extra(jjs_HL(2, :), (mextra+1)/2)
       END DO
 
 
