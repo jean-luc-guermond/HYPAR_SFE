1fe28cc79e6f59394d7e328e0cde3e8f49836950
Victor <botez@lisn.fr>
2026-06-09 20:39:49 +0200

    line truncation variables.cmake

--- TEMPLATE_CMAKE/variables.cmake
+++ TEMPLATE_CMAKE/variables.cmake
@@ -1,9 +1,9 @@
 # Variables for Whistler
 #==== Mandatory for any application ====
 set(HYPAR_SFE_DIR "my_path_to_hypar_sfe/HYPAR_SFE")
-set(debug_bounds "-Wall -Wextra -fimplicit-none -finit-real=snan -finit-integer=-999999 -fbounds-check")
+set(debug_bounds "-Wall -Wextra -fimplicit-none -finit-real=snan -finit-integer=-999999 -fbounds-check -ffree-line-length-none")
 #set(debug_bounds "-Wall -fimplicit-none -fbounds-check")
-set(release_bounds "-O3")
+set(release_bounds "-O3 -ffree-line-length-none")
 set(FE_dim "1") #1 or 2
 set(static_library OFF)
 
